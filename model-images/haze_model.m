d48f6dacf695ef6b9fbabd6d2eac351d19777a1a
Alankar Kotwal <alankarkotwal13@gmail.com>
2015-08-11 17:38:06 +0530

    add hazemodel

---
+++ model-images/haze_model.m
@@ -0,0 +1,14 @@
+J = im2double(imread('2.png'));
+temp = im2double(imread('tx_map_out.png'));
+
+A = J;
+t = J;
+A(:, :, 1) = double(ones(128, 116));
+A(:, :, 2) = 203/255*double(ones(128, 116));
+A(:, :, 3) = 203/255*double(ones(128, 116));
+t(:, :, 1) = temp;
+t(:, :, 2) = temp;
+t(:, :, 3) = temp;
+
+I = (1 - t).*A + t.*J;
+imshowpair(I, J, 'montage');
